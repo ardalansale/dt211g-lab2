372644109468ab301f6e1ba5ad86407937b0f374
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2026-03-08 22:31:15 +0100

    Kommentera
    Börja sortering

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,36 @@
 const url = "https://webbutveckling.miun.se/files/ramschema.json"
 
-async function fetchSchedule() {
+let allCourses = []; // En tom lista där alla kurser sparas när de hämtats
+let sortColumn = "code"; // Vilken kolumn som det sorteras på just nu
+let sortAscending = true; // Sortering på bokstavsordning där true = A till Ö och false = Ö till A
+
+// Funktion som hämtar kurser från webbadressen ovan
+async function fetchSchedule() { 
     try {
         const response = await fetch(url);
         const data = await response.json();
-        showTable(data);
+
+        // Spara alla kurser i tabellen
+        allCourses = data;
+
+        // Visa kurserna i tabellen
+        showTable(allCourses);
+
     } catch (error) {
         console.log("Något har gått fel:", error);
     }
 }
 
+// En funktion som skriver ut kurserna i tabellen
 function showTable(courses) {
     const tbody = document.getElementById("table-body");
+
+    // Tömmer tabellen innan den fylls igen
     tbody.innerHTML = "";
 
+    // Går igenom varje kurs i listan, en i taget
     courses.forEach(function(course) {
+        // Skapa en ny rad med kursens infromation/data och för att sedan lägga till den i tabellen
         tbody.innerHTML += `
             <tr>
                 <td>${course.code}</td>
